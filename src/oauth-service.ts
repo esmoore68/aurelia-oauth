933bedbd86e495c20dfa89c3b29141fc996ee1a5
esmoore68 <esmoore68@gmail.com>
2017-04-21 12:21:57 -0500

    Added support for storing token in localStorage and for pushState

--- src/oauth-service.ts
+++ src/oauth-service.ts
@@ -9,208 +9,210 @@ import { objectAssign } from './oauth-polyfills';
 const OAUTH_STARTPAGE_STORAGE_KEY: string = 'oauth.startPage';
 
 export interface OAuthConfig {
-    loginUrl: string;
-    logoutUrl: string;
-    clientId: string;
-    logoutRedirectParameterName?: string;
-    scope?: string;
-    state?: string;
-    redirectUri?: string;
-    alwaysRequireLogin?: boolean;
-    autoTokenRenewal?: boolean;
+  loginUrl: string;
+  logoutUrl: string;
+  clientId: string;
+  logoutRedirectParameterName?: string;
+  scope?: string;
+  state?: string;
+  redirectUri?: string;
+  alwaysRequireLogin?: boolean;
+  autoTokenRenewal?: boolean;
+  applicationUsesPushState?: boolean;
 }
 
 @autoinject()
 export class OAuthService {
 
-    public config: OAuthConfig;
+  public config: OAuthConfig;
 
-    private defaults: OAuthConfig;
+  private defaults: OAuthConfig;
 
-    public static get LOGIN_SUCCESS_EVENT(): string { return 'oauth:loginSuccess'; }
-    public static get INVALID_TOKEN_EVENT(): string { return 'oauth:invalidToken'; }
+  public static get LOGIN_SUCCESS_EVENT(): string { return 'oauth:loginSuccess'; }
+  public static get INVALID_TOKEN_EVENT(): string { return 'oauth:invalidToken'; }
 
-    constructor(
-        private oAuthTokenService: OAuthTokenService,
-        private urlHashService: UrlHashService,
-        private localStorageService: LocalStorageService,
-        private eventAggregator: EventAggregator) {
+  constructor(
+    private oAuthTokenService: OAuthTokenService,
+    private urlHashService: UrlHashService,
+    private localStorageService: LocalStorageService,
+    private eventAggregator: EventAggregator) {
 
-        this.defaults = {
-            loginUrl: null,
-            logoutUrl: null,
-            clientId: null,
-            logoutRedirectParameterName: 'post_logout_redirect_uri',
-            scope: null,
-            state: null,
-            alwaysRequireLogin: false,
-            autoTokenRenewal: true
-        };
+    this.defaults = {
+      loginUrl: null,
+      logoutUrl: null,
+      clientId: null,
+      logoutRedirectParameterName: 'post_logout_redirect_uri',
+      scope: null,
+      state: null,
+      alwaysRequireLogin: false,
+      autoTokenRenewal: true,
+      applicationUsesPushState: false
+    };
+  }
+
+  public configure = (config: OAuthConfig): OAuthConfig => {
+    if (this.config) {
+      throw new Error('OAuthProvider already configured.');
     }
 
-    public configure = (config: OAuthConfig): OAuthConfig => {
-        if (this.config) {
-            throw new Error('OAuthProvider already configured.');
+    // Remove trailing slash from urls.
+    if (config.loginUrl.substr(-1) === '/') {
+      config.loginUrl = config.loginUrl.slice(0, -1);
+    }
+
+    if (config.logoutUrl.substr(-1) === '/') {
+      config.logoutUrl = config.logoutUrl.slice(0, -1);
+    }
+
+    // Extend default configuration.
+    this.config = objectAssign(this.defaults, config);
+
+    // Redirect is set to current location by default
+    const existingHash = window.location.hash;
+    let pathDefault = window.location.href;
+
+    // Remove not needed parts from urls.
+    if (existingHash) {
+      pathDefault = pathDefault.replace(existingHash, '');
+    }
+
+    if (pathDefault.substr(-1) === '#') {
+      pathDefault = pathDefault.slice(0, -1);
+    }
+
+    this.config.redirectUri = config.redirectUri || pathDefault;
+
+    return config;
+  };
+
+  public isAuthenticated = (): boolean => {
+    return <any>this.oAuthTokenService.getToken();
+  };
+
+  public login = (): void => {
+    window.location.href = this.getRedirectUrl();
+  };
+
+  public logout = (): void => {
+    const redirectUrl = `${this.config.logoutUrl}?` +
+      `${this.config.logoutRedirectParameterName}=${encodeURIComponent(this.config.redirectUri)}`;
+
+    window.location.href = redirectUrl;
+    this.oAuthTokenService.removeToken();
+  };
+
+  public loginOnStateChange = (toState): boolean => {
+    if (toState && this.isLoginRequired(toState) && !this.isAuthenticated() && !this.getTokenDataFromUrl()) {
+
+      if (this.localStorageService.isStorageSupported()) {
+        let url = window.location.href;
+
+        if (!window.location.hash) {
+          url = this.getBaseRouteUrl();
         }
 
-        // Remove trailing slash from urls.
-        if (config.loginUrl.substr(-1) === '/') {
-            config.loginUrl = config.loginUrl.slice(0, -1);
-        }
+        this.localStorageService.set<string>(OAUTH_STARTPAGE_STORAGE_KEY, url);
+      }
 
-        if (config.logoutUrl.substr(-1) === '/') {
-            config.logoutUrl = config.logoutUrl.slice(0, -1);
-        }
+      this.login();
 
-        // Extend default configuration.
-        this.config = objectAssign(this.defaults, config);
+      return true;
+    }
 
-        // Redirect is set to current location by default
-        const existingHash = window.location.hash;
-        let pathDefault = window.location.href;
+    return false;
+  };
 
-        // Remove not needed parts from urls.
-        if (existingHash) {
-            pathDefault = pathDefault.replace(existingHash, '');
-        }
+  public setTokenOnRedirect = (): void => {
+    const tokenData = this.getTokenDataFromUrl();
 
-        if (pathDefault.substr(-1) === '#') {
-            pathDefault = pathDefault.slice(0, -1);
-        }
+    if (!this.isAuthenticated() && tokenData) {
+      this.oAuthTokenService.setToken(tokenData);
 
-        this.config.redirectUri = config.redirectUri || pathDefault;
+      if (this.localStorageService.isStorageSupported() && this.localStorageService.get(OAUTH_STARTPAGE_STORAGE_KEY)) {
+        const startPage = this.localStorageService.get<string>(OAUTH_STARTPAGE_STORAGE_KEY);
 
-        return config;
-    };
+        this.localStorageService.remove(OAUTH_STARTPAGE_STORAGE_KEY);
+        window.location.href = startPage;
+      } else {
+        // Redirect to the base application route
+        window.location.href = this.getBaseRouteUrl();
+      }
 
-    public isAuthenticated = (): boolean => {
-        return <any>this.oAuthTokenService.getToken();
-    };
+      this.eventAggregator.publish(OAuthService.LOGIN_SUCCESS_EVENT, tokenData);
 
-    public login = (): void => {
-        window.location.href = this.getRedirectUrl();
-    };
+      if (this.config.autoTokenRenewal) {
+        this.setAutomaticTokenRenewal();
+      }
+    }
+  };
 
-    public logout = (): void => {
-        const redirectUrl = `${this.config.logoutUrl}?` +
-            `${this.config.logoutRedirectParameterName}=${encodeURIComponent(this.config.redirectUri)}`;
+  private isLoginRequired = (state): boolean => {
+    const routeHasConfig = state.settings && state.settings.requireLogin !== undefined;
+    const routeRequiresLogin = routeHasConfig && state.settings.requireLogin ? true : false;
 
-        window.location.href = redirectUrl;
-        this.oAuthTokenService.removeToken();
-    };
+    return routeHasConfig ? routeRequiresLogin : this.config.alwaysRequireLogin;
+  };
 
-    public loginOnStateChange = (toState): boolean => {
-        if (toState && this.isLoginRequired(toState) && !this.isAuthenticated() && !this.getTokenDataFromUrl()) {
+  private getTokenDataFromUrl = (hash?: string): OAuthTokenData => {
+    const hashData = this.urlHashService.getHashData(hash);
+    const tokenData = this.oAuthTokenService.createToken(hashData);
 
-            if (this.localStorageService.isStorageSupported()) {
-                let url = window.location.href;
+    return tokenData;
+  };
 
-                if (!window.location.hash) {
-                    url = this.getBaseRouteUrl();
-                }
+  private getBaseRouteUrl = (): string => {
+    return this.config.applicationUsesPushState ? window.location.href.split('#')[0] + '#' : window.location.origin + '/#/';
+  }
 
-                this.localStorageService.set<string>(OAUTH_STARTPAGE_STORAGE_KEY, url);
-            }
+  private getSimpleNonceValue = (): string => {
+    return ((Date.now() + Math.random()) * Math.random()).toString().replace('.', '');
+  }
 
-            this.login();
+  private getRedirectUrl() {
+    let redirectUrl = `${this.config.loginUrl}?` +
+      `response_type=${this.oAuthTokenService.config.name}&` +
+      `client_id=${encodeURIComponent(this.config.clientId)}&` +
+      `redirect_uri=${encodeURIComponent(this.config.redirectUri)}&` +
+      `nonce=${encodeURIComponent(this.getSimpleNonceValue())}`;
 
-            return true;
-        }
+    if (this.config.scope) {
+      redirectUrl += `&scope=${encodeURIComponent(this.config.scope)}`;
+    }
 
-        return false;
-    };
+    if (this.config.state) {
+      redirectUrl += `&state=${encodeURIComponent(this.config.state)}`;
+    }
 
-    public setTokenOnRedirect = (): void => {
-        const tokenData = this.getTokenDataFromUrl();
+    return redirectUrl;
+  }
 
-        if (!this.isAuthenticated() && tokenData) {
+  private setAutomaticTokenRenewal() {
+    const tokenExpirationTime = this.oAuthTokenService.getTokenExpirationTime() * 1000;
+
+    setTimeout(() => {
+      const iFrame = document.createElement('iframe');
+      iFrame.src = this.getRedirectUrl();
+      iFrame.style.display = 'none';
+      iFrame.onload = (event) => {
+        try {
+          const hashWithNewToken = iFrame.contentWindow.location.hash;
+          document.body.removeChild(iFrame);
+
+          const tokenData = this.getTokenDataFromUrl(hashWithNewToken);
+
+          if (tokenData) {
             this.oAuthTokenService.setToken(tokenData);
-
-            if (this.localStorageService.isStorageSupported() && this.localStorageService.get(OAUTH_STARTPAGE_STORAGE_KEY)) {
-                const startPage = this.localStorageService.get<string>(OAUTH_STARTPAGE_STORAGE_KEY);
-
-                this.localStorageService.remove(OAUTH_STARTPAGE_STORAGE_KEY);
-                window.location.href = startPage;
-            } else {
-                // Redirect to the base application route
-                window.location.href = this.getBaseRouteUrl();
-            }
-
-            this.eventAggregator.publish(OAuthService.LOGIN_SUCCESS_EVENT, tokenData);
-
-            if (this.config.autoTokenRenewal) {
-                this.setAutomaticTokenRenewal();
-            }
+            this.setAutomaticTokenRenewal();
+          }
+        } catch (ex) {
+          // iFrame.contentWindow can fail when an iframe loads identity server login page
+          // but this page will not redirect back to the app url waiting for the user to login in
+          // this behaviour my occur i.e. when login page authentication cookies expire
+          document.body.removeChild(iFrame);
         }
-    };
+      };
 
-    private isLoginRequired = (state): boolean => {
-        const routeHasConfig = state.settings && state.settings.requireLogin !== undefined;
-        const routeRequiresLogin = routeHasConfig && state.settings.requireLogin ? true : false;
-
-        return routeHasConfig ? routeRequiresLogin : this.config.alwaysRequireLogin;
-    };
-
-    private getTokenDataFromUrl = (hash?: string): OAuthTokenData => {
-        const hashData = this.urlHashService.getHashData(hash);
-        const tokenData = this.oAuthTokenService.createToken(hashData);
-
-        return tokenData;
-    };
-
-    private getBaseRouteUrl = (): string => {
-        return window.location.origin + '/#/';
-    }
-
-    private getSimpleNonceValue = (): string => {
-        return ((Date.now() + Math.random()) * Math.random()).toString().replace('.', '');
-    }
-
-    private getRedirectUrl() {
-        let redirectUrl = `${this.config.loginUrl}?` +
-            `response_type=${this.oAuthTokenService.config.name}&` +
-            `client_id=${encodeURIComponent(this.config.clientId)}&` +
-            `redirect_uri=${encodeURIComponent(this.config.redirectUri)}&` +
-            `nonce=${encodeURIComponent(this.getSimpleNonceValue())}`;
-
-        if (this.config.scope) {
-            redirectUrl += `&scope=${encodeURIComponent(this.config.scope)}`;
-        }
-
-        if (this.config.state) {
-            redirectUrl += `&state=${encodeURIComponent(this.config.state)}`;
-        }
-
-        return redirectUrl;
-    }
-
-    private setAutomaticTokenRenewal() {
-        const tokenExpirationTime = this.oAuthTokenService.getTokenExpirationTime() * 1000;
-
-        setTimeout(() => {
-            const iFrame = document.createElement('iframe');
-            iFrame.src = this.getRedirectUrl();
-            iFrame.style.display = 'none';
-            iFrame.onload = (event) => {
-                try {
-                    const hashWithNewToken = iFrame.contentWindow.location.hash;
-                    document.body.removeChild(iFrame);
-
-                    const tokenData = this.getTokenDataFromUrl(hashWithNewToken);
-
-                    if (tokenData) {
-                        this.oAuthTokenService.setToken(tokenData);
-                        this.setAutomaticTokenRenewal();
-                    }
-                } catch (ex) {
-                    // iFrame.contentWindow can fail when an iframe loads identity server login page
-                    // but this page will not redirect back to the app url waiting for the user to login in
-                    // this behaviour my occur i.e. when login page authentication cookies expire
-                    document.body.removeChild(iFrame);
-                }
-            };
-
-            document.body.appendChild(iFrame);
-        }, tokenExpirationTime);
-    }
+      document.body.appendChild(iFrame);
+    }, tokenExpirationTime);
+  }
 }
--- src/oauth-token-service.ts
+++ src/oauth-token-service.ts
@@ -1,126 +1,147 @@
+import LocalStorageService from './local-storage-service';
 import { autoinject } from 'aurelia-dependency-injection';
 
 import JwtTokenService, { JwtClaims } from './jwt-token-service';
 import { objectAssign } from './oauth-polyfills';
 
 export interface OAuthTokenConfig {
-    name: string;
-    urlTokenParameters?: {
-        idToken: string;
-        tokenType?: string;
-    };
-    expireOffsetSeconds?: number;
+  name: string;
+  urlTokenParameters?: {
+    idToken: string;
+    tokenType?: string;
+  };
+  expireOffsetSeconds?: number;
+  persistTokenToLocalStorage: false;
+  localStorageTokenKey: string;
 }
 
 export interface OAuthTokenData {
-    token: string;
-    tokenType: string;
-    expiresAt: number;
-    jwtClaims?: JwtClaims;
+  token: string;
+  tokenType: string;
+  expiresAt: number;
+  jwtClaims?: JwtClaims;
 }
 
 @autoinject()
 export class OAuthTokenService {
 
-    public config: OAuthTokenConfig;
+  public config: OAuthTokenConfig;
+  private tokenData: OAuthTokenData;
 
-    private tokenData: OAuthTokenData;
+  constructor(private jwtTokenService: JwtTokenService, private localStorageService: LocalStorageService) {
+    this.config = {
+      name: 'id_token',
+      urlTokenParameters: {
+        idToken: 'id_token',
+        tokenType: 'token_type'
+      },
+      expireOffsetSeconds: 60,
+      persistTokenToLocalStorage: false,
+      localStorageTokenKey: 'oauthAccessToken'
+    };
+  }
 
-    constructor(private jwtTokenService: JwtTokenService) {
-        this.config = {
-            name: 'id_token',
-            urlTokenParameters: {
-                idToken: 'id_token',
-                tokenType: 'token_type'
-            },
-            expireOffsetSeconds: 60
-        };
+  public configure = (config: OAuthTokenConfig): OAuthTokenConfig => {
+
+    // Extend default configuration with supplied config data
+    if (config.urlTokenParameters) {
+      config.urlTokenParameters = objectAssign(this.config.urlTokenParameters, config.urlTokenParameters);
     }
 
-    public configure = (config: OAuthTokenConfig): OAuthTokenConfig => {
+    this.config = objectAssign(this.config, config);
 
-        // Extend default configration with supplied config data
-        if (config.urlTokenParameters) {
-            config.urlTokenParameters = objectAssign(this.config.urlTokenParameters, config.urlTokenParameters);
-        }
+    return config;
+  };
 
-        this.config = objectAssign(this.config, config);
+  public createToken = (urlTokenData: any): OAuthTokenData => {
+    const token = urlTokenData[this.config.urlTokenParameters.idToken];
+    const tokenType = urlTokenData[this.config.urlTokenParameters.tokenType] || 'Bearer';
 
-        return config;
+    if (!token) {
+      return null;
+    }
+
+    const claims: JwtClaims = this.jwtTokenService.getJwtClaims(token);
+    const issuedTime = claims.nbf ? claims.nbf : claims.iat;
+    const expirationTime = claims.exp - issuedTime;
+
+    return {
+      token: token,
+      tokenType: tokenType,
+      expiresAt: this.getTimeNow() + expirationTime,
+      jwtClaims: claims
     };
+  };
 
-    public createToken = (urlTokenData: any): OAuthTokenData => {
-        const token = urlTokenData[this.config.urlTokenParameters.idToken];
-        const tokenType = urlTokenData[this.config.urlTokenParameters.tokenType] || 'Bearer';
+  public setToken = (data: OAuthTokenData): OAuthTokenData => {
+    if (this.config.persistTokenToLocalStorage && this.localStorageService.isStorageSupported()) {
+      if (data) {
+        this.localStorageService.set(this.config.localStorageTokenKey, data);
+      } else {
+        this.localStorageService.remove(this.config.localStorageTokenKey);
+      }
+    }
+    return this.tokenData = data;
+  };
 
-        if (!token) {
-            return null;
-        }
+  public getToken = (): OAuthTokenData => {
 
-        const claims: JwtClaims = this.jwtTokenService.getJwtClaims(token);
-        const issuedTime = claims.nbf ? claims.nbf : claims.iat;
-        const expirationTime = claims.exp - issuedTime;
+    if (!this.tokenData && this.config.persistTokenToLocalStorage && this.localStorageService.isStorageSupported()) {
+      return this.tokenData = this.localStorageService.get<OAuthTokenData>(this.config.localStorageTokenKey);
+    } else {
+      return this.tokenData;
+    }
+  };
 
-        return {
-            token: token,
-            tokenType: tokenType,
-            expiresAt: this.getTimeNow() + expirationTime,
-            jwtClaims: claims
-        };
-    };
+  public getIdToken = (): string => {
+    return this.getToken() ? this.getToken().token : undefined;
+  };
 
-    public setToken = (data: OAuthTokenData): OAuthTokenData => {
-        return this.tokenData = data;
-    };
+  public getAuthorizationHeader = (): string => {
+    if (!(this.getTokenType() && this.getIdToken())) {
+      return '';
+    }
 
-    public getToken = (): OAuthTokenData => {
-        return this.tokenData;
-    };
+    const tokenType = this.getTokenType().charAt(0).toUpperCase() + this.getTokenType().substr(1);
 
-    public getIdToken = (): string => {
-        return this.getToken() ? this.getToken().token : undefined;
-    };
+    return `${tokenType} ${this.getIdToken()}`;
+  };
 
-    public getAuthorizationHeader = (): string => {
-        if (!(this.getTokenType() && this.getIdToken())) {
-            return '';
-        }
+  public getTokenType = (): string => {
+    return this.getToken() ? this.getToken().tokenType : undefined;
+  };
 
-        const tokenType = this.getTokenType().charAt(0).toUpperCase() + this.getTokenType().substr(1);
+  public getTokenExpirationTime = (): number => {
+    const tokenRenewalOffsetSeconds = 30;
+    const expireOffset = this.config.expireOffsetSeconds + tokenRenewalOffsetSeconds;
 
-        return `${tokenType} ${this.getIdToken()}`;
-    };
+    return (this.tokenData.expiresAt - this.getTimeNow() - expireOffset);
+  };
 
-    public getTokenType = (): string => {
-        return this.getToken() ? this.getToken().tokenType : undefined;
-    };
+  public removeToken = (): OAuthTokenData => {
+    if (this.tokenData && this.config.persistTokenToLocalStorage && this.localStorageService.isStorageSupported()) {
+      this.localStorageService.remove(this.config.localStorageTokenKey);
+    }
 
-    public getTokenExpirationTime = (): number => {
-        const tokenRenewalOffsetSeconds = 30;
-        const expireOffset = this.config.expireOffsetSeconds + tokenRenewalOffsetSeconds;
+    return this.tokenData = null;
+  };
 
-        return (this.tokenData.expiresAt - this.getTimeNow() - expireOffset);
-    };
+  public isTokenValid = (): boolean => {
+    const token = this.getToken();
 
-    public removeToken = (): OAuthTokenData => {
-        return this.tokenData = null;
-    };
+    if (!token) {
+      return false;
+    }
 
-    public isTokenValid = (): boolean => {
-        const token = this.getToken();
+    const timeNow = this.getTimeNow();
+    const expiresAt = token.expiresAt;
+    const isValid = (expiresAt && (expiresAt > timeNow + this.config.expireOffsetSeconds));
 
-        if (!token) {
-            return false;
-        }
+    return isValid;
+  };
 
-        const timeNow = this.getTimeNow();
-        const expiresAt = token.expiresAt;
-        const isValid = (expiresAt && (expiresAt > timeNow + this.config.expireOffsetSeconds));
+  private getTimeNow = (): number => {
+    return Math.round(new Date().getTime() / 1000.0);
+  };
 
-        return isValid;
-    };
-
-    private getTimeNow = (): number => {
-        return Math.round(new Date().getTime() / 1000.0);
-    };
 }
