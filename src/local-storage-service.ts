0600d38fdda1ed8639915579c59a2c1775a41e52
esmoore68 <esmoore68@gmail.com>
2017-04-21 12:20:32 -0500

    Add files via upload
    Updated IsStorageAvailable to actually test storage read/write, and to handle checking also for sessionStorage

--- src/local-storage-service.ts
+++ src/local-storage-service.ts
@@ -1,17 +1,29 @@
 export default class LocalStorageService {
-    public isStorageSupported(): boolean {
-        return window.localStorage !== undefined;
+  public isStorageSupported(storageType: string = null): boolean {
+    // 'sessionStorage' is another possible value
+    storageType = storageType || 'localStorage';
+    try {
+      const storage = window[storageType];
+      // Safari in private mode supplies a storage object with 0 quota, 
+      // so ensure that we can actually read to and write from storage
+      const x = '__storage_test__';
+      storage.setItem(x, x);
+      storage.removeItem(x);
+      return true;
+    } catch (e) {
+      return false;
     }
+  }
 
-    public set<T>(key: string, object: T): void {
-        window.localStorage.setItem(key, JSON.stringify(object));
-    }
+  public set<T>(key: string, object: T): void {
+    window.localStorage.setItem(key, JSON.stringify(object));
+  }
 
-    public get<T>(key: string): T {
-        return JSON.parse(window.localStorage.getItem(key));
-    }
+  public get<T>(key: string): T {
+    return JSON.parse(window.localStorage.getItem(key));
+  }
 
-    public remove(key: string): void {
-        window.localStorage.removeItem(key);
-    }
+  public remove(key: string): void {
+    window.localStorage.removeItem(key);
+  }
 }
